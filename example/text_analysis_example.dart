e7e733eac7ca1d6fc53c267987def109a1729a2c
GerhardMalan <gmalan@gmconsult.com.au>
2022-09-02 06:14:57 +1000

    Initial setup

---
+++ example/text_analysis_example.dart
@@ -0,0 +1,6 @@
+import 'package:text_analysis/text_analysis.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/text_analysis.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library text_analysis;
+
+export 'src/text_analysis_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
