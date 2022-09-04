5b89d898a15669a5eac0400151dd0e8cbe3c852a
GerhardMalan <gmalan@gmconsult.com.au>
2022-09-05 09:43:34 +1000

    dart doc, reworked Sentence and Token

--- lib/src/text_source/sentence.dart
+++ lib/src/text_source/sentence.dart
@@ -9,29 +9,15 @@ import 'package:text_analysis/text_analysis.dart';
 ///
 /// In addition to the [source] text, the [Sentence] object also enumerates
 /// its [tokens].
-abstract class Sentence {
+class Sentence {
   //
 
-  /// Factory constructor instantiates a [Sentence] from [source] and [tokens].
-  factory Sentence(source, List<Token> tokens) => _SentenceImpl(source, tokens);
+  /// Instantiates a [Sentence] from [source] and [tokens].
+  const Sentence(this.source, this.tokens);
 
   /// The source text of the [Sentence].
-  String get source;
-
-  /// All the tokens in the [Sentence].
-  List<Token> get tokens;
-}
-
-/// Implementation class for [Sentence].
-class _SentenceImpl implements Sentence {
-//
-
-  /// Instantiates a const [_SentenceImpl]
-  const _SentenceImpl(this.source, this.tokens);
-
-  @override
   final String source;
 
-  @override
+  /// All the tokens in the [Sentence].
   final List<Token> tokens;
 }
