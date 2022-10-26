112de28d3e8cfa46c33ea0d74684334882f3e46d
GerhardMalan <gmalan@gmconsult.com.au>
2022-10-26 20:35:22 +1100

    version: 0.21.1

--- lib/src/text_analyzer/_index.dart
+++ lib/src/text_analyzer/_index.dart
@@ -2,6 +2,6 @@
 // Copyright ©2022, GM Consult Pty Ltd
 // All rights reserved
 
-export 'co_occurrence_graph.dart';
+export 'term_co_occurrence_graph.dart';
 export 'text_analysis_extensions.dart';
 export 'text_analyzer.dart';
